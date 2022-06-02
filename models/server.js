5d3ae27be996441eae6a260d2137892470218bb0
Daniel Benitez <dabenitez@hotmail.com>
2022-06-01 23:00:42 -0400

    Listo para subir a produccion

--- models/server.js
+++ models/server.js
@@ -30,7 +30,7 @@ class Server {
 
     listen() {
         this.app.listen(this.port, () => {
-            console.log( 'Servidor corriendo en puerto ', this.port );
+            console.log( 'Servidor corriendo en puerto', this.port );
         });
     }
 }
